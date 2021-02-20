d98ed8801c366fc0b7cfe235746a959d6b7cfc53
Berkay Dogukan Urhan <dogu.1769@gmail.com>
2021-02-21 01:50:49 +0300

    create dasboard screen wip

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -2,37 +2,40 @@ import React from 'react'
 import styles from './card.module.css'
 import cn from 'classnames'
 
-const Title = ({ children }) => children;
-Title.displayName = 'Title';
+const Title = ({ children }) => children
+Title.displayName = 'Title'
 
+const Body = ({ children }) => children
+Body.displayName = 'Body'
 
-const Body = ({ children }) => children;
-Body.displayName = 'Body';
+const Warning = ({ children, className }) => children
+Warning.displayName = 'Warning'
 
-const Warning = ({ children }) => children;
-Warning.displayName = 'Warning';
-
-index.Title = Title;
-index.Body = Body;
-index.Warning = Warning;
-
-
+index.Title = Title
+index.Body = Body
+index.Warning = Warning
 
 export default function index({ className, children }) {
+  const title = React.Children.map(children, (child) =>
+    child.type.displayName === 'Title' ? child : null
+  )
+  const body = React.Children.map(children, (child) =>
+    child.type.displayName === 'Body' ? child : null
+  )
+  const warning = React.Children.map(children, (child) =>
+    child.type.displayName === 'Warning' ? child : null
+  )
+  console.log(warning)
+  return (
+    <div className={cn(styles.card, className)}>
+      {title.length > 0 && <div className={styles.title}>{title}</div>}
+      <div className={styles.body}>{body}</div>
 
-    const title = React.Children.map(children, child => child.type.displayName === 'Title' ? child : null)
-    const body = React.Children.map(children, child => child.type.displayName === 'Body' ? child : null)
-    const warning = React.Children.map(children, child => child.type.displayName === 'Warning' ? child : null)
-
-
-
-
-    return (
-        <div className={cn(styles.card, className)}>
-            <div className={styles.title}>{title}</div>
-            <div className={styles.body}>{body}</div>
-            <div className={styles.warning}>{warning}</div>
-
+      {warning.length > 0 && (
+        <div className={cn(styles.warning, warning[0].props.className)}>
+          {warning}
         </div>
-    )
+      )}
+    </div>
+  )
 }
--- src/pages/CreateDashboard/index.js
+++ src/pages/CreateDashboard/index.js
@@ -1,5 +1,31 @@
-import React from 'react'
+import Card from 'components/Card'
+import Input from 'components/Input'
+import React, { useState } from 'react'
+import styles from './cdashboard.module.css'
 
-export default function index() {
-  return <div></div>
+export default function Index() {
+  const [team, setTeam] = useState(null)
+  const [tables, setTables] = useState([''])
+
+  return (
+    <div>
+      <Card className={styles.teamCard}>
+        <Card.Body>
+          <Input placeholder="Takım adını belirtiniz " />
+        </Card.Body>
+        <Card.Warning className={styles.teamCardWarning}>
+          <div className="warning-content">
+            Dashboard ekranında daha fazla takım ekleyebilirsiniz
+          </div>
+        </Card.Warning>
+      </Card>
+      {tables.map((x, i) => {
+        return (
+          <Card className={styles.tableCard} key={i}>
+            <Card.Title>{i + 1}. Kart</Card.Title>
+          </Card>
+        )
+      })}
+    </div>
+  )
 }
